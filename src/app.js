3456ea58bc85427559af2be331a162657f74e6e4
Artem Rimarchuk <1patrik@mail.ru>
2020-02-11 23:42:05 +0300

    add app to github

---
+++ src/app.js
@@ -0,0 +1,69 @@
+import { Question } from './question'
+import { isValid, createModal } from './utils';
+import { getAuthForm, authWithEmailAndPassword } from './auth';
+import './styles.css';
+
+const form = document.getElementById('form');
+const modalBtn = document.getElementById('modal-btn');
+const input = form.querySelector('.question-input');
+const submitBtn = form.querySelector('#submit');
+
+window.addEventListener('load', Question.renderList)// отрендеривает страницу с вопросами после обновления страницы
+modalBtn.addEventListener('click', openModal)
+form.addEventListener('submit', submitFormHandler);
+input.addEventListener('input', () => {
+    submitBtn.disabled = !isValid(input.value)
+})
+
+function submitFormHandler(event) {
+    event.preventDefault();
+
+    if (isValid(input.value)) {
+        const question = {
+            text: input.value.trim(),
+            date: new Date().toJSON()
+        }
+
+        submitBtn.disabled = true
+        //Async request to server to save question
+        Question.create(question).then(() => {
+            input.value = '';
+            input.className = '';
+            submitBtn.disabled = false;
+        })
+        //console.log('Question', question)
+
+    }
+
+}
+
+function openModal() {
+    createModal('Авторизация', getAuthForm())
+    document.getElementById('auth-form').addEventListener('submit', authFormHandler, { once: true })
+}
+
+function authFormHandler(event) {
+    event.preventDefault();
+
+    const btn = event.target.querySelector('button')
+    const email = event.target.querySelector('.email').value
+    const password = event.target.querySelector('.password').value
+
+    btn.disabled = true;
+
+    authWithEmailAndPassword(email, password)
+
+        .then(token => {
+            return Question.fetch(token)
+        })
+        .then(renderModalAfterAuth)
+        .then(() => btn.disabled = false)
+}
+
+function renderModalAfterAuth(content) {
+    if (typeof content === 'string') {
+        createModal('Ошибка', content)
+    } else {
+        createModal('Список вопросов', Question.listToHTML(content))
+    }
+}
